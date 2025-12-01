59e556d51a4cc60618a297f22a0f00e39c9788c2
Mert Ibrahim Biyikli 12241947 <a12247947@univie.ac.at>
2025-12-01 15:46:28 +0100

    added hash.h

--- src/solver/solver.h
+++ src/solver/solver.h
@@ -23,7 +23,7 @@ public:
     std::vector<double> oracle_running_times;
     std::vector<double> pure_oracle_running_times;
     int iteration_count = 0;
-    std::unordered_map<std::pair<int, int> , std::unordered_map<std::pair<int, int>, double >> f_e_st;
+    std::unordered_map<std::pair<int, int> , std::unordered_map<std::pair<int, int>, double, PairHash >> f_e_st;
 
     std::vector<std::vector<double>> m_routingTable;
     ObliviousRoutingSolver() = default;
