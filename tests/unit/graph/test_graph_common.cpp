6ebb9d0d7b95c726341e723a0e3233321e4dfc7e
Mert Ibrahim Biyikli 12241947 <a12247947@univie.ac.at>
2026-06-08 13:09:55 +0200

    added cmath

--- tests/unit/graph/test_graph_common.cpp
+++ tests/unit/graph/test_graph_common.cpp
@@ -4,6 +4,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_approx.hpp>
 #include <vector>
+#include <cmath>
 
 #include "graph_test_helpers.h"
 #include "catch2/catch_template_test_macros.hpp"
